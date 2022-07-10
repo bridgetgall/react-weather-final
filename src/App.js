09af1bd8d9197d08140722b30092e76b9831985b
bridgetgall <bridgetgalla@gmail.com>
2022-07-10 11:28:11 -0500

    Added GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <footer>
+        This project was coded by Bridget Gallagher and is{" "}
+        <a
+          href="https://github.com/bridgetgall/react-weather-final"
+          target="_blank"
+        >
+          open-sourced.
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
