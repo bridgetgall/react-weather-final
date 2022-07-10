d73acee1c4419fc25d8674817d8b61465d8a56a6
bridgetgall <bridgetgalla@gmail.com>
2022-07-10 11:56:50 -0500

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <footer>
-        This project was coded by Bridget Gallagher and is{" "}
-        <a
-          href="https://github.com/bridgetgall/react-weather-final"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          open-sourced.
-        </a>
-      </footer>
+      <div className="container">
+        <footer>
+          This project was coded by Bridget Gallagher and is{" "}
+          <a
+            href="https://github.com/bridgetgall/react-weather-final"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            open-sourced.
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
