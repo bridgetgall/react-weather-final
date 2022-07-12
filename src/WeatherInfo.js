985b9b7df3200ebf59fd88c72a62e7cfd6e25fe1
bridgetgall <bridgetgalla@gmail.com>
2022-07-12 18:52:17 -0500

    Update WeatherInfo.js

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -15,11 +15,15 @@ export default function WeatherInfo(props) {
       </ul>
       <div className="row mt-3">
         <div className="col-6">
-          <WeatherIcon code={props.data.icon} size={52} />
-
-          <WeatherTemperature celcius={props.data.temperature} />
+          <div className="clearfix">
+            <div className="float-left">
+              <WeatherIcon code={props.data.icon} size={52} />
+            </div>
+            <div className="float-left">
+              <WeatherTemperature celcius={props.data.temperature} />
+            </div>
+          </div>
         </div>
-
         <div className="col-6">
           <ul>
             <li>Humidity: {Math.round(props.data.humidity)}%</li>
